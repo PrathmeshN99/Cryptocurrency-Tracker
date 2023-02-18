daeb0c1e8a4c7bd740145ff10658880babf72b38
PrathmeshN99 <90515944+PrathmeshN99@users.noreply.github.com>
2023-02-18 15:07:52 +0530

    Commit

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyBUdfFIY4xvMnXiigZ00aBlAVYdStxTODE",
+  authDomain: "crypto-tracker-3e107.firebaseapp.com",
+  projectId: "crypto-tracker-3e107",
+  storageBucket: "crypto-tracker-3e107.appspot.com",
+  messagingSenderId: "210487231695",
+  appId: "1:210487231695:web:17717af912a444ca5da3ba",
+};
+
+export default firebaseConfig;
+
+
